2659ee4b37b27633174853baf7c9bbf782bdd30f
Seaborn Lee <seabornlee.cn@gmail.com>
2016-06-16 16:29:33 +0800

    Refactor: DRY

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ var express = require('express');
 var app = express();
 var products = require('./products.js');
 var maxIdUtils = require('./max-id-utils.js');
+const components = [
+    'getOneProduct',
+    'getAllProducts',
+    'addProduct',
+    'deleteProduct',
+    'updataProducts'
+];
 
 maxIdUtils.initMaxIdFile();
 
-app.use('/', require('./getOneProduct.js'));
-app.use('/', require('./getAllProducts.js'));
-app.use('/', require('./addProduct.js'));
-app.use('/', require('./deleteProduct.js'));
-app.use('/', require('./updataProducts.js'));
+components.forEach(function (component) {
+    app.use('/', require('./' + component + '.js'));
+});
 
 app.listen(8010, function () {
     console.log('server start');
