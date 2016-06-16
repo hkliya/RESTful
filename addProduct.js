51a88fff13c5b641bd48bca4eedcc7a355ccf79c
Seaborn Lee <seabornlee.cn@gmail.com>
2016-06-16 21:07:43 +0800

    Refactor: re-organize

--- addProduct.js
+++ addProduct.js
@@ -7,23 +7,18 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
 router.post('/', function (req, res) {
-
-    fs.readFile('./products.json', 'utf-8', function (err, fileContent) {
-        if (err) {
-            res.status(500).end();
-
-            return;
-        }
-
-        var data = JSON.parse(fileContent);
-
-        if (isValid(req.body)) {
-            addProduct(data, req.body, res);
-        }
-        else {
-            res.sendStatus(400);
-        }
-    });
+    if (isValid(req.body)) {
+        console.log(req.body);
+        saveProduct(req.body, function(successful, product) {
+            if (successful) {
+                res.status(201).json(product);
+            } else {
+                res.status(500);
+            }
+        });
+    } else {
+        res.sendStatus(400);
+    }
 });
 
 function isValid(product) {
@@ -47,59 +42,78 @@ function hasRightType(product) {
         && typeof product.price == "number";
 }
 
-function addProduct(productsData, product, res) {
-
-    fs.readFile('./max-id.json', 'utf-8', function (err, data) {
-        if (err) {
-            res.sendStatus(500);
-
-            return;
+function saveProduct(product, callback) {
+    updateMaxId(function (successfully, id) {
+        if (successfully) {
+            product.id = id;
+            updateProducts(product, callback);
+        } else {
+            callback(false);
         }
-
-        data = JSON.parse(data);
-
-        data.maxId++;
-
-        writeMaxId(data, res);
-
-        var maxId = data.maxId;
-        var item = {
-            "id": maxId,
-            "barcode": product.barcode,
-            "name": product.name,
-            "unit": product.unit,
-            "price": product.price
-        };
-        productsData.push(item);
-
-        writeFile(productsData, res);
     });
 }
 
-function writeMaxId(data, res) {
-
-    fs.writeFile('./max-id.json', JSON.stringify(data), function (err) {
-        if (err) {
-
-            res.sendStatus(500);
-
-            return;
+function updateMaxId(callback) {
+    readMaxId(function (successfully, id) {
+        if (successfully) {
+            writeMaxId(id + 1, callback);
+        } else {
+            callback(false);
         }
-
-        return;
     });
 }
 
-function writeFile(productsData, res) {
-
-    fs.writeFile('./products.json', JSON.stringify(productsData), function (err) {
+function readMaxId(callback) {
+    fs.readFile('./max-id.json', 'utf-8', function (err, fileContent) {
         if (err) {
-
-            res.status(404);
-
+            callback(false);
             return;
         }
-        res.status(201).json(productsData[productsData.length - 1]);
+
+        var data = JSON.parse(fileContent);
+        callback(true, data.maxId);
+    })
+}
+
+function writeMaxId(id, callback) {
+    fs.writeFile('./max-id.json', JSON.stringify({"maxId": id}), function (err) {
+        if (err) {
+            callback(false);
+            return;
+        }
+
+        callback(true, id);
+    });
+}
+
+function updateProducts(product, callback) {
+    readProducts(function(successfully, products) {
+        if (successfully) {
+            products.push(product);
+            writeProducts(products, callback);
+        } else {
+            callback(false);
+        }
+    });
+}
+
+function readProducts(callback) {
+    fs.readFile('./products.json', 'utf-8', function (err, fileContent) {
+        if (err) {
+            callback(false);
+        } else {
+            callback(true, JSON.parse(fileContent));
+        }
+    });
+}
+
+function writeProducts(products, callback) {
+    fs.writeFile('./products.json', JSON.stringify(products), function (err) {
+        if (err) {
+            callback(false);
+        } else {
+            callback(true, products[products.length - 1]);
+        }
     });
 }
 
