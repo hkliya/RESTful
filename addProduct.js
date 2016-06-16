4ea9dc4948845328da52814dc9ac47a37c8d61c5
Seaborn Lee <seabornlee.cn@gmail.com>
2016-06-16 22:20:07 +0800

    Clean code

--- addProduct.js
+++ addProduct.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var productUtils = require('./product-utils.js');
-var fs = require("fs");
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
